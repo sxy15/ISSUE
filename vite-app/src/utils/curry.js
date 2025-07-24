6504e4b0a1680ff3a198c8a480e3f3ad15f21561
sxy15 <sxy_19921115@163.com>
2025-07-24 21:47:05 +0800

    feat: curry

---
+++ vite-app/src/utils/curry.js
@@ -0,0 +1,9 @@
+export function curry(fn) {
+    return function curried(...args) {
+        if (args.length >= fn.length) {
+            return fn(...args)
+        }
+
+        return (...args2) => curried(...args, ...args2)
+    }
+}
